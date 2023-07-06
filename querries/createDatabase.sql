9d9b7a6b0b0e72542cdad3fd9ea226cae6bdda39
hiba <hibaejaz06@gmail.com>
2023-07-06 20:29:16 +0300

    employee table created

--- querries/createDatabase.sql
+++ querries/createDatabase.sql
@@ -43,3 +43,44 @@ CREATE TABLE IF NOT EXISTS public.team_project
         ON UPDATE CASCADE
         ON DELETE CASCADE
 )
+
+-- Table: public.employee
+
+-- DROP TABLE IF EXISTS public.employee;
+
+CREATE TABLE IF NOT EXISTS public.employee
+(
+    employee_id integer NOT NULL DEFAULT nextval('employee_employee_id_seq'::regclass),
+    first_name character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying(15) COLLATE pg_catalog."default" NOT NULL,
+    hire_date date NOT NULL,
+    hourly_salary numeric(10,2) NOT NULL,
+    title_id integer,
+    manager_id integer,
+    team_id integer,
+    CONSTRAINT employee_pkey PRIMARY KEY (employee_id),
+    CONSTRAINT manager_id FOREIGN KEY (manager_id)
+        REFERENCES public.employee (employee_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT team_id FOREIGN KEY (team_id)
+        REFERENCES public.team (team_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
+    CONSTRAINT title_id FOREIGN KEY (title_id)
+        REFERENCES public.title (title_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
+-- Constraint: manager_id
+
+-- ALTER TABLE IF EXISTS public.employee DROP CONSTRAINT IF EXISTS manager_id;
+
+ALTER TABLE IF EXISTS public.employee
+    ADD CONSTRAINT manager_id FOREIGN KEY (manager_id)
+    REFERENCES public.employee (employee_id) MATCH SIMPLE
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+    NOT VALID;
+CREATE INDEX IF NOT EXISTS fki_manager_id
+    ON public.employee(manager_id);
